08316b1e3293a019a830fcf11fbf9e9e266a20bc
Hendreik <slvl2@list.ru>
2024-08-07 20:03:44 +0300

    add module_5_hard

---
+++ module5hard.py
@@ -0,0 +1,14 @@
+# module5hard.py
+"""
+по модулю: "Классы и объекты."
+
+Цель: Применить знания полученные в модуле, решив задачу повышенного уровня сложности.
+
+Задание "Свой YouTube":
+3 класса: UrTube, Video, User.
+"""
+import Data_structure.DB_whs as dbw
+
+user_cls = dbw.Db_Users()
+video_cls = dbw.Videos()
+ut_cls = dbw.UrTube()
