56567171da6bf8e55d24847f05ea828648b517d6
Hendreik <slvl2@list.ru>
2024-09-15 13:34:27 +0300

    ver 1

--- module_9_1.py
+++ module_9_1.py
@@ -16,32 +16,48 @@ int_list - список из чисел (int, float)
 """
 int_list = [1, 2.02, 3, 4.4, -5, 6]
 
-
 def func1(x):
 	return x + 1
-
 
 def func2(x):
 	return x * 2
 
-
 def func3(x):
 	return x * x
 
-
 funcs = [func1, func2, func3]
+
+
+def _all_func(int_list, *functions):
+	results = {}
+	for f in functions:
+		print(f.__name__)
+		for i in int_list:
+			results[i] = f(i)
+		# print(f(i))
+		print(results)
+
+	return results
 
 
 def apply_all_func(int_list, *functions):
 	results = {}
 	for f in functions:
-		print(f)
-		for i in int_list:
-			results[i] = f(i)
-		# print(f(i))
-	print(results)
+		results[f.__name__] = f(int_list)
 
 	return results
 
 
-result = apply_all_func(int_list, *funcs)
+###
+result = _all_func(int_list, *funcs)
+print('----')
+###
+"""
+min - принимает список, возвращает минимальное значение из него.
+max - принимает список, возвращает максимальное значение из него.
+len - принимает список, возвращает кол-во элементов в нём.
+sum - принимает список, возвращает сумму его элементов.
+sorted - принимает список, возвращает новый отсортированный список на основе переданного.
+"""
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
