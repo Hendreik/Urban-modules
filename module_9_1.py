b3c77cee786c32d5a26d4d4be3231543e0cc0ab2
Hendreik <slvl2@list.ru>
2024-09-15 12:25:16 +0300

    new

---
+++ module_9_1.py
@@ -0,0 +1,47 @@
+# module_9_1.py
+"""
+"Введение в функциональное программирование"
+Напишите функцию apply_all_func(int_list, *functions), которая принимает параметры:
+int_list - список из чисел (int, float)
+*functions - неограниченное кол-во функций (которые применимы к спискам, состоящим из чисел)
+Эта функция должна:
+Вызвать каждую функцию к переданному списку int_list
+Возвращать словарь, где ключом будет название вызванной функции, а значением - её результат работы со списком int_list.
+Пункты задачи:
+В функции apply_all_func создайте пустой словарь results.
+Переберите все функции из *functions.
+При переборе функций записывайте в словарь results результат работы этой функции под ключом её названия.
+Верните словарь results.
+Запустите функцию apply_all_func, передав в неё список из чисел и набор других функций.
+"""
+int_list = [1, 2.02, 3, 4.4, -5, 6]
+
+
+def func1(x):
+	return x + 1
+
+
+def func2(x):
+	return x * 2
+
+
+def func3(x):
+	return x * x
+
+
+funcs = [func1, func2, func3]
+
+
+def apply_all_func(int_list, *functions):
+	results = {}
+	for f in functions:
+		print(f)
+		for i in int_list:
+			results[i] = f(i)
+		# print(f(i))
+	print(results)
+
+	return results
+
+
+result = apply_all_func(int_list, *funcs)
