c5a1387c407b5583f45303113c97a158eb01939d
Hendreik <slvl2@list.ru>
2024-09-22 00:05:25 +0300

    ver 1

---
+++ module_10_2.py
@@ -0,0 +1,51 @@
+# module_10_2.py
+"""
+Потоки на классах
+	name (str)
+	power (int)
+количество врагов уменьшается на power текущего рыцаря.
+"""
+import threading
+from threading import Thread
+from time import sleep
+
+
+class Knight(Thread):
+	ENEMY = 100
+	_st1 = ", на нас напали!"
+
+	def __init__(self, name, power):
+		self.name_ = name
+		self._power = power
+		super().__init__()
+
+	def run(self):
+		try:
+			print(f"{self.name_}{self._st1}")
+			enemy = self.ENEMY
+			days = 0
+			while enemy > 0:
+				enemy -= self._power
+				days += 1
+				print(f"{self.name_} сражается {days} days..., осталось {enemy} воинов.")
+				sleep(1)
+			st3 = f"{self.name_} одержал победу спустя {days} дней(дня)!"
+			print(st3)
+
+		except Exception as e:
+			print(e.__str__())
+
+
+# Создание класса
+first_knight = Knight('Sir Lancelot', 10)
+second_knight = Knight("Sir Galahad", 20)
+# Запуск потоков и остановка текущего
+first_knight.start()
+second_knight.start()
+first_knight.join(3)
+second_knight.join(3)
+# Вывод строки об окончании сражения
+print(first_knight.name, first_knight.is_alive(), " alive")
+print(second_knight.__getstate__(), second_knight.is_alive())
+
+assert threading.active_count() == 2
