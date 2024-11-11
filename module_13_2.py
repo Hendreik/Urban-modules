ffe8cd65407ef963a42f544a7d69669a49978451
Hendreik <slvl2@list.ru>
2024-11-11 04:44:15 +0300

    ver.1

---
+++ module_13_2.py
@@ -0,0 +1,26 @@
+# module_13_2.py
+"""
+ "Бот поддержки (Начало)"
+"""
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api="7768252079:AAGl4c19PpytBXJ1CsdV04pOJ4zdqWKVKvs"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=["Start"])
+async def Start_message(message):
+	print("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_messages(message):
+	print("Введите команду /start, чтобы начать общение.")
+
+
+
+if __name__ == "__main__":
+	executor.start_polling(dp, skip_updates=True)
+
+
