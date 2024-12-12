a80a233b89a9b3beaca2e1f94d46cffc54b8c391
Hendreik <slvl2@list.ru>
2024-12-12 05:03:31 +0300

    release 2

--- module_16_2.py
+++ module_16_2.py
@@ -30,9 +30,9 @@ uvicorn module_16_2:app --reload
 
 
 @app.get("/user/{user_id}")
-async def read_user(user_id: int = Path(..., title='Enter User ID',
-										ge=1, le=100,
-										description='only positive integer 1...100 ')):
+async def read_user(
+		user_id: int = Path(..., title='Enter User ID', ge=1, le=100, description='only positive integer 1...100 ')
+):
 	return {"User: ": f"Вы вошли как пользователь № <{user_id}>"}
 
 
@@ -43,13 +43,10 @@ async def read_user(user_id: int = Path(..., title='Enter User ID',
 # Описания для username и age - 'Enter username' и 'Enter age' соответственно.
 # Примеры для username и age - 'UrbanUser' и '24' соответственно. (можете подставить свои примеры не противоречащие валидации).
 @app.get("/user/{username}/{age}")
-async def _user(username: Annotated[str, Path(min_length=5, max_length=20,
-											  regex="^[A-Za-z\\s]+$",
-											  title='Enter username',
-											  description='Enter username in english')],
-				age: Annotated[int, Path(ge=18, le=120,
-										 title='Enter age',
-										 description="Enter age. only positive integer 18—120 ")]
+async def _user(username: Annotated[
+	str, Path(min_length=5, max_length=20, regex="^[A-Za-z\\s]+$", title='Enter username',description='Enter username in english')],
+				age: Annotated[
+					int, Path(ge=18, le=120, title='Enter age', description="Enter age. only positive integer 18—120 ")]
 				):
 	"""
 	Выводим пользователя.
