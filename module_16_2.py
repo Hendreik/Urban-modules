ac859660ddc65d199e0fe410cb0e50ad75150406
Hendreik <slvl2@list.ru>
2024-12-13 13:13:50 +0300

    release 1.3

--- module_16_2.py
+++ module_16_2.py
@@ -31,27 +31,35 @@ uvicorn module_16_2:app --reload
 
 @app.get("/user/{user_id}")
 async def read_user(
-		user_id: int = Path(..., title='Enter User ID', ge=1, le=100, description='only positive integer 1...100 ')
+user_id: int = Path(..., title='Enter User ID', ge=1, le=100, description='only positive integer 1...100',example=1)
 ):
 	return {"User: ": f"Вы вошли как пользователь № <{user_id}>"}
 
 
-# '/user' замените на '/user/{username}/{age}' - функция, выполняемая по этому маршруту, принимает аргументы username и age, для которых необходимо написать следующую валидацию:
+# '/user' замените на '/user/{username}/{age}' - функция, выполняемая по этому маршруту, принимает аргументы username и
+# age, для которых необходимо написать следующую валидацию:
 # username - строка, age - целое число.
 # username ограничение по длине: больше или равно 5 и меньше либо равно 20.
 # age ограничение по значению: больше или равно 18 и меньше либо равно 120.
 # Описания для username и age - 'Enter username' и 'Enter age' соответственно.
 # Примеры для username и age - 'UrbanUser' и '24' соответственно. (можете подставить свои примеры не противоречащие валидации).
 @app.get("/user/{username}/{age}")
-async def _user(username: Annotated[
-	str, Path(min_length=5, max_length=20, regex="^[A-Za-z\\s]+$", title='Enter username',description='Enter username in english')],
-	age: Annotated[
-	int, Path(ge=18, le=120, title='Enter age', description="Enter age. only positive integer 18—120 ")]
-				):
+async def _user(
+username: Annotated[str, Path(..., min_length=5, max_length=20, regex="^[A-Za-z\\s]+$", title='Enter username',
+description='Enter username in english', example='UrbanUser')],
+age: Annotated[int, Path(..., ge=18, le=120, title='Enter age',
+description="Enter age. only positive integer 18—120 ", example=20)]
+):
 	"""
 	Выводим пользователя.
 	- **name**: логин
 	- **age**: возраст
 
 	"""
-	return {"Информация о пользователе. Имя, Возраст: ": f"user: {username} age: {age}"}
+	return {"Информация о пользователе. Имя, Возраст: ": f"user: {username}, age: {age}"}
+
+
+if __name__ == "__main__":
+	import uvicorn
+
+	uvicorn.run(app, host="localhost", port=8001)
