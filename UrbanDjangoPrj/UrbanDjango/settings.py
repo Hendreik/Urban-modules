14ecc8b6497ca85d9257130b5af09ebf8cf71937
Hendreik <slvl2@list.ru>
2025-01-07 05:58:27 +0300

    ver 1.5

--- UrbanDjangoPrj/UrbanDjango/settings.py
+++ UrbanDjangoPrj/UrbanDjango/settings.py
@@ -15,7 +15,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 	'task2',
 	'task3',
+	'task4',
+	'task5',
 	'App'
 ]
 #'django.app',
@@ -125,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR,'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- UrbanDjangoPrj/UrbanDjango/urls.py
+++ UrbanDjangoPrj/UrbanDjango/urls.py
@@ -20,15 +20,23 @@ from  django.views.generic import TemplateView
 
 from task2.views import index, index2
 from task3.views import index as i, index1 as i1, index2 as i2, index3 as i3
+from task4.views import index as i4, index1 as i41, index2 as i42, index3 as i43, index4 as i44, base
+from task5.views import index as run5
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('', i),
-	path('1/', i1),
-	path('2/', TemplateView.as_view(template_name='third_task\\task2.html')),
-	path('3/', TemplateView.as_view(template_name='third_task\\task3.html')),
+	path('', i4),
+	path('base/', base),
+	path('1/', i41),
+	path('2/', i42.as_view()),
+	path('3/', i43),
+	path('4/', i44),
+	path('41/', TemplateView.as_view(template_name='fourth_task\\task4.html')),
 	path('t2', index),
 	path('t2/1', index2),
+
+	path('5/', run5),
+
 ]
 
 
